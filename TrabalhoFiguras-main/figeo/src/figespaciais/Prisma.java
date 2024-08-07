6ae6a217e806ad236cbe368063ee82ab2638305e
Miguel Angelo <160639066+miangelw@users.noreply.github.com>
2024-08-06 22:03:41 -0300

    Update Prisma.java

--- TrabalhoFiguras-main/figeo/src/figespaciais/Prisma.java
+++ TrabalhoFiguras-main/figeo/src/figespaciais/Prisma.java
@@ -1,5 +1,33 @@
 package figespaciais;
 
 public class Prisma {
+        public double areabase;
+        public double perimetrobase;
+        public double volume;
+        public dobule comprimento;
+        public double altura;
+        public double largura;
+        public double superficie;
+
+    public void calcAreabase() {
+            areabase = comprimento * largura;
+            System.out.println(areabase);
+    }
+    public void calcPerimetroBase() { 
+            perimetrobase = 2 * (comprimento + largura);
+            System.out.println(perimetrobase);
+    }
+    public void calcVolume() {
+            volume = areabase * altura
+            System.out.println(volume);
+    }
+    public void calcSuperficie() {
+            superficie = 2 * (areabase + perimetro * altura);
+            System.out.println(areasuperficie);
+    }
+
+
+
 
 }
+
